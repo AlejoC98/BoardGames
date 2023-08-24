291bb7dea5b886b4be2a4296fd53e739def769bd
Alejandro Gomez <alegomezc98@icloud.com>
2023-08-24 03:45:14 -0600

    I implemented some styles to the sidebar an the topbar, I also started the process to pull the board games information from the API

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { Game } from '../interfaces/game';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,11 @@ import { Injectable } from '@angular/core';
 export class GameService {
 
   constructor(private http: HttpClient) { }
+
+  searchByName(keyword: string): Observable<Game> {
+    const alterKeyWord = keyword.replace(/\s/g, '+');
+    return this.http.get<Game[]>(`https://api.boardgameatlas.com/api/search?name=${alterKeyWord}&client_id=${environment.boardgameAPI}`).pipe(
+      map(response => response['games'])
+    )
+  };
 }
